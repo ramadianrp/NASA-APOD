12278f706bebdaa917a86fe803ce5089f5a34880
ramadianrp <ramadianrp@gmail.com>
2024-03-22 11:51:43 +0700

    update

--- server/routers/index.js
+++ server/routers/index.js
@@ -9,7 +9,7 @@ const errHandler = require('../middlewares/errHandler')
 
 // login-regis
 router.post("/login", AuthController.login);
-router.post("/google-login", AuthController,AuthController.googleLogin)
+router.post("/google-login", AuthController.googleLogin)
 router.use(authen);
 router.post("/add-user", authorAdmin, AuthController.addUser);
 
